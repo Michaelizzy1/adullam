70a7f85393008a3699999dbe464f0284408de3cc
Michaelizzy1 <michaelizzy2@gmail.com>
2023-03-11 02:34:18 +0100

    another commit

--- adullam/settings.py
+++ adullam/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'adullam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '6Ar6sEsqZL4SooLf2seD',
+        'HOST': 'containers-us-west-154.railway.app',
+        'PORT': '6435',
     }
 }
 
